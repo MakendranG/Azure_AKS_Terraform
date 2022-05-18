ac37116f150903fdf368a885889d6b471362fdfe
MakendranG <74320000+MakendranG@users.noreply.github.com>
2022-05-18 13:35:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ resource "random_id" "log_analytics_workspace_name_suffix" {
   byte_length = 8
 }
 
-resource "azurerm_log_analytics_workspace" "foxulogs" {
+resource "azurerm_log_analytics_workspace" "kcdchennailogs" {
   name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = var.log_analytics_workspace_sku
 }
 
-resource "azurerm_log_analytics_solution" "foxulogs" {
+resource "azurerm_log_analytics_solution" "kcdchennailogs" {
   solution_name         = "ContainerInsights"
   location              = var.location
   resource_group_name   = azurerm_resource_group.rg.name
-  workspace_resource_id = azurerm_log_analytics_workspace.foxulogs.id
-  workspace_name        = azurerm_log_analytics_workspace.foxulogs.name
+  workspace_resource_id = azurerm_log_analytics_workspace.kcdchennailogs.id
+  workspace_name        = azurerm_log_analytics_workspace.kcdchennailogs.name
 
   plan {
     publisher = "Microsoft"
@@ -47,7 +47,7 @@ resource "azurerm_kubernetes_cluster" "ak8s" {
   addon_profile {
     oms_agent {
       enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.foxulogs.id
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.kcdchennailogs.id
     }
   }
 
