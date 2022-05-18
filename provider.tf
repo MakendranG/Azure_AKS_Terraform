9a9948ca4942c0cd1db56a03c189ba6b08b5973a
MakendranG <74320000+MakendranG@users.noreply.github.com>
2022-05-18 13:15:44 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
 
-  required_version = ">=0.12"
+  required_version = ">=1.1"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>2.4"
     }
   }
 
